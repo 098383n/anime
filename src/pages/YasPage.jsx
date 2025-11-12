bb328c5927a70fba395d84d283682ef8c25cab42
Lohachka <kkmarmokuu@gmail.com>
2025-11-12 16:41:40 +0500

    Rename Yaspage component to YasPage

--- src/pages/YasPage.jsx
+++ src/pages/YasPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Yas from '../copanents/Yas/Yas'
 
-const Yaspage = () => {
+const YasPage = () => {
   return (
     <div>
       <Yas/>
@@ -9,4 +9,4 @@ const Yaspage = () => {
   )
 }
 
-export default Yaspage
+export default YasPage
